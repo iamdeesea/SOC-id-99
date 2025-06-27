0813cac575f89e33aeed9fb29d759fbe7d12cf62
iamdeesea <formaldhruv@gmail.com>
2025-06-27 16:05:38 +0530

    Create PythonBasics.py

---
+++ week1/PythonBasics.py
@@ -0,0 +1,45 @@
+# name = input("what is your name")
+# print("Hello" + name)
+# a = input("Enter a: ")
+# b = input("Enter b: ")
+# age = 2020 - int(birth_year)
+# print(age)
+# sum =float(a) + float(b)
+# print("The sum is " + str(sum))
+# course = 'Python for Beginners'
+# print(course.upper())
+# print(course.find('Python'))
+# print('Python' in course)
+# print(10//3)
+# x=3==2
+# print(x)
+# temperature =35
+# if temperature >30:{
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# }
+# weight = input("Weight: ")
+# unit = input("(K)g or (L)bs: ")
+# if unit.upper()=="K":
+#     converted = float(weight)/0.45
+#     print("Weight in Lbs: "+str(converted))
+# elif unit.upper()=="L":
+#     converted = float(weight)*0.45
+#     print("Weight in Kg: "+str(converted))
+# i=1
+# while i<=1_0:
+#     print(i*"*")
+#     i+=1
+# names = ["John","Bob","Mosh","Sam","Mary"]
+# names[0]="Jon"
+# print(names[0:4])
+# numbers=["1","2","3","4","5","6"]
+# numbers.append(6)
+# numbers.insert(0,-5)
+# print(numbers)
+# print(len(numbers))
+# for item in numbers:
+#     print(item)
+# numbers=range(1,100,2)
+# for number in numbers:
+#     print(number)
